e402f60394517fe4c76d74eb764b9ee0d2e3c63c
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2017-11-04 18:39:02 +1000

    First commit.

---
+++ src/rpc/testString.java
@@ -0,0 +1,35 @@
+package rpc;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testString {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	 @Test
+	 public void testSubString(){
+	   String str = new String("This is a unit test.");
+	   assertEquals("unit", str.substring(10, 14));
+	 }	
+	
+}
